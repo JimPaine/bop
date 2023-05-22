b12e8055f7859c78997f1d615e1446a5c1aa8edb
Jim Paine <17758778+JimPaine@users.noreply.github.com>
2023-05-22 10:02:44 +0100

    fixed index tracking

--- src/lexer.rs
+++ src/lexer.rs
@@ -46,14 +46,27 @@ impl Lexer {
         let mut tokens = Vec::new();
 
         for i in 0..self.source.len() {
+            if self.current == self.source.len() {
+                tokens.push(Token { token_type: TokenType::EOF, lexeme: '\0'.to_string(), line: self.line});
+                break;
+            }
             self.start = self.current;
-            let c = self.source[i];
+            let c = self.source[self.start];
             match c {
-                _ => {
-                    if Self::is_alphanumeric(c) {
-                        tokens.push(self.get_identifier());
-                    }
-                }
+                ' ' => {
+                    self.forward();
+                    continue;
+                },
+                '\n' => {
+                    tokens.push(Token { token_type: TokenType::EOL, lexeme: '\t'.to_string(), line: self.line});
+                    self.line += 1;
+                    self.forward();
+                },
+                '=' => {
+                    tokens.push(Token { token_type: TokenType::ASSIGN, lexeme: '='.to_string(), line: self.line});
+                    self.forward();
+                },
+                _ => tokens.push(self.get_identifier())
             }
         }
 
@@ -65,28 +78,27 @@ impl Lexer {
     }
 
     fn get_identifier(&mut self) -> Token {
-        while Self::is_alphanumeric(self.peek()) {
+        while Self::is_alphanumeric(self.source[self.current]) {
             self.forward();
+            if self.current >= self.source.len() {
+                break;
+            }
         }
-        let raw = self.source[self.start..self.current].into_iter().collect::<String>();
-        println!("{raw}");
+        let raw = if self.start == self.current {
+            self.source[self.current].to_string()
+        } else {
+            String::from_iter(self.source[self.start..self.current].iter())
+        };
+
         Token {
             lexeme: raw,
             line: self.line,
             token_type: TokenType::IDENTIFIER
         }
-
-    }
-
-    fn peek(&self) -> char {
-        if self.current >= self.source.len() {
-            return '\0';
-        };
-        self.source[self.current + 1]
     }
 
     fn forward(&mut self) {
-        self.current += 1;
+        self.current += 1
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod lexer;
 use lexer::Lexer;
 
 fn main() {
-    let mut lexer = Lexer::new(String::from("a = b"));
+    let mut lexer = Lexer::new(String::from("abc = x\ndef = xyz"));
 
     for token in lexer.scan().iter() {
         println!("{} {:?} {}", token.lexeme, token.token_type, token.line);
